b3bc6743c9776606f16623564de1da2bd70bf95d
Kome Nathaniel <77756722+nattcodes@users.noreply.github.com>
2023-10-29 13:06:27 +0100

    k

--- public/js/main.js
+++ public/js/main.js
@@ -8,5 +8,32 @@ const assign1 = {
     title: "Project",
     grade: 1
 };
-console.log(updateAssignment(assign1, { grade: 95 }));
-const assignGraded = updateAssignment(assign1, { grade: 95 });
+const assignGraded = updateAssignment(assign1, { grade: 99 });
+// console.log(assignGraded)
+//Required and Read Only
+const record = (assign) => {
+    return assign;
+};
+const recordV = Object.assign(Object.assign({}, assignGraded), { verified: true });
+// console.log(record)
+// console.log(recordV)
+//Record
+const colorMap = {
+    red: 'ff0000',
+    green: '00ff00',
+    blue: '0000ff'
+};
+const finalGrades = {
+    Mark: 'A',
+    John: 'F'
+};
+const pickResult = {
+    studentId: 'kartos',
+    grade: 99
+};
+const omitResult = {
+    grade: 99,
+    title: 'tyujhn'
+};
+// console.log(pickResult)
+// console.log(omitResult)
--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,62 @@ type Assignment = {
     verified?: boolean
 }
 
+
 const updateAssignment = (assign: Assignment, propsToUpdate: Partial<Assignment>): Assignment => {
     return {...assign, ...propsToUpdate}
 }
 
-const assign1: Assignment = {
+const assign1: Assignment = {    
     studentId: "nattcode1234",
     title: "Project",
     grade: 1
 }
 
-console.log(updateAssignment(assign1, {grade:95}))
-const assignGraded: Assignment = updateAssignment(assign1, {grade: 95})
+const assignGraded: Assignment = updateAssignment(assign1, {grade: 99})
+// console.log(assignGraded)
+
+
+
+//Required and Read Only
+
+const record = (assign: Required<Assignment>): Assignment => {
+    return assign
+}
+
+const recordV: Readonly<Assignment> = {
+    ...assignGraded, verified: true
+}
+// console.log(record)
+// console.log(recordV)
+
+
+//Record
+const colorMap: Record<string, string> = {
+    red: 'ff0000',
+    green: '00ff00',
+    blue: '0000ff'
+}
+
+type Students = 'Mark' | "John"
+type Grades = 'A' | 'B' | 'C' | 'D' | 'F'
+
+const finalGrades: Record< Students, Grades> = {
+    Mark: 'A',
+    John: 'F'
+}
+// Pick and Omit
+
+type AssignmentResult = Pick<Assignment, "studentId" | "grade">
+type AssignPreview = Omit<Assignment, "studentId">;
+
+const pickResult: AssignmentResult = {
+    studentId: 'kartos',
+    grade: 99
+    
+}
+const omitResult: AssignPreview = {
+    grade: 99,
+    title: 'tyujhn'
+}
+// console.log(pickResult)
+// console.log(omitResult)
